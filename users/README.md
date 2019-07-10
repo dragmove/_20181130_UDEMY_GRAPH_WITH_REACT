bced2f0404a7166ae85b678764c715fd391546f4
김현석 <dragmove@naver.com>
2019-07-10 10:43:40 +0900

    feat: Add CompanyType

---
+++ users/README.md
@@ -0,0 +1,29 @@
+# _20181130_UDEMY_GRAPH_WITH_REACT
+Test GraphQL
+
+## About
+* Test GraphQL
+
+
+## Run
+
+```sh
+npm i
+
+npm run json:server // run json-server. // https://www.npmjs.com/package/json-server  
+// Test http://localhost:3000/users/23  
+// Test http://localhost:3000/companies/2/users  
+
+npm run dev // run server.js by nodmon. connect http://localhost:9001 Web App
+```
+
+
+## Contact
+* @Website : http://www.dragmove.xyz
+* @Blog : https://blog.naver.com/dragmove
+* @LinkedIn : https://www.linkedin.com/in/hyun-seok-kim-99748295/
+* @E-mail : dragmove@gmail.com
+
+
+## License
+[MIT license](http://danro.mit-license.org/).
--- users/schema/schema.js
+++ users/schema/schema.js
@@ -1,17 +1,38 @@
-const _ = require('lodash');
+// const _ = require('lodash');
+const axios = require('axios');
 const graphql = require('graphql');
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
 
 // dummy data in DB
-const users = [{ id: '23', firstName: 'Bill', age: 20 }, { id: '47', firstName: 'Samantha', age: 21 }];
+/*
+const users = [
+  { id: '23', firstName: 'Bill', age: 20 }, 
+  { id: '47', firstName: 'Samantha', age: 21 }
+];
+*/
 
 // scheme example
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString }
+  }
+});
+
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: {
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
-    age: { type: GraphQLInt }
+    age: { type: GraphQLInt },
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`).then(res => res.data);
+      }
+    }
   }
 });
 
@@ -26,7 +47,8 @@ const RootQuery = new GraphQLObjectType({
       },
       resolve(parentValue, args) {
         // return a actual piece of data from database or data store.
-        return _.find(users, { id: args.id });
+        // return _.find(users, { id: args.id });
+        return axios.get(`http://localhost:3000/users/${args.id}`).then(res => res.data);
       }
     }
   }
